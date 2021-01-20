d2e112c858ba7dc2dde449bcd381621ec741759b
Bob Makowski <tritonpi-git0@yahoo.com>
2021-01-20 17:32:50 -0600

    save and Person.all worked but without validation checks via annotations on domain object

--- src/main/groovy/sci/category/geovisit/domain/Person.groovy
+++ src/main/groovy/sci/category/geovisit/domain/Person.groovy
@@ -3,17 +3,24 @@ package sci.category.geovisit.domain
 import grails.gorm.annotation.Entity
 import groovy.transform.CompileStatic
 
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 @CompileStatic
 @Entity
 class Person{
-    @NotNull
-    @Size(min=5, max=50)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    Long id
+//    @NotNull
+//    @Size(min = 5, max = 50)
     String firstName
-    @Size(min=5, max=50)
+//    @Size(min = 5, max = 50)
     String lastName
-    @NotNull
+//    @NotNull
     Boolean isCool = false
 }
---
+++ src/test/groovy/sci/category/geovisit/domain/PersonSpec.groovy
@@ -0,0 +1,19 @@
+package sci.category.geovisit.domain
+
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+
+@MicronautTest
+class PersonSpec extends Specification{
+
+    void 'test person.save and Person.all'() {
+        given:
+            Map map = [ firstName: "bob" , lastName: "mak" , isCool: true ]
+            Person p = new Person(map)
+            p.save()
+            List plist = Person.all
+        expect:
+            assert map
+            assert plist
+    }
+}
