416bc13d66c6e5a7e3fae38256b15a53eb194ad5
Bob Makowski <tritonpi-git0@yahoo.com>
2021-01-27 16:30:03 -0600

    logging via Slf4J

--- src/test/groovy/sci/category/geovisit/MicronautDemoSpec.groovy
+++ src/test/groovy/sci/category/geovisit/MicronautDemoSpec.groovy
@@ -1,19 +1,25 @@
 package sci.category
 
+import groovy.util.logging.Slf4j
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import spock.lang.Specification
 import javax.inject.Inject
 
 @MicronautTest
+@Slf4j
 class MicronautDemoSpec extends Specification {
 
     @Inject
     EmbeddedApplication<?> application
 
     void 'test it works'() {
+        given:
+        log.debug("PING!")
         expect:
         application.running
     }
-
+    private static final Logger log = LoggerFactory.getLogger(MicronautDemoSpec.class);
 }
