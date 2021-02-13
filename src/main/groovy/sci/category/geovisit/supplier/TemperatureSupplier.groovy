fb478dc0d2dfcac5cf58639ac12729a40d6ed8e8
Bob Makowski <tritonpi-git0@yahoo.com>
2021-02-12 18:38:55 -0600

    refactor temperature supplier to use temperature builder and model and they measure temperature via closure. worked

--- src/main/groovy/sci/category/geovisit/supplier/TemperatureSupplier.groovy
+++ src/main/groovy/sci/category/geovisit/supplier/TemperatureSupplier.groovy
@@ -1,10 +1,6 @@
 package sci.category.geovisit.supplier
 
 import groovy.util.logging.Slf4j
-import io.micronaut.core.type.Argument
-import io.micronaut.http.HttpRequest
-import io.micronaut.http.HttpResponse
-import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.RxStreamingHttpClient
 import org.jgrapht.Graph
 import org.jgrapht.traverse.BreadthFirstIterator
@@ -14,6 +10,7 @@ import sci.category.geovisit.contract.BuildContract
 import sci.category.geovisit.contract.SupplierContract
 import sci.category.geovisit.domain.OrgAddress
 import sci.category.geovisit.domain.Temperature
+import sci.category.geovisit.dsl.TemperatureModel
 
 @Slf4j
 class TemperatureSupplier implements SupplierContract<List<Temperature>>{
@@ -50,6 +47,8 @@ class TemperatureSupplier implements SupplierContract<List<Temperature>>{
 
         @Override
         TemperatureSupplier build() {
+            final Map config = [httpClient: getClient()]
+            TemperatureModel model = new TemperatureModel(config)
             OrgTreeSupplier orgTreeSupplier = getOrgTreeSupplier()
             Graph graph = orgTreeSupplier.get()
             OrgAddress root = orgTreeSupplier.getRoot()
@@ -59,58 +58,15 @@ class TemperatureSupplier implements SupplierContract<List<Temperature>>{
                 Map payload = oa.payload
                 Map cityAndState = [city: payload.city, state: payload.state]
                 if (!cityAndState.values().contains(null)) {
-                    Temperature temp = getTemperatureByCityAndByStateViaHttp(cityAndState)
+                    Closure c = {
+                        get cityAndState
+                    }
+                    Temperature temp = model.measure c
                     temp
                 }
             }
             builderConfig[FactoryKey.Bootstrap] = tempsList
             TemperatureSupplier supplier = new TemperatureSupplier(this)
-        }
-
-        private Temperature getTemperatureByCityAndByStateViaHttp(Map cityAndState) {
-            final def weatherApiKey = '9ecd4d849bec4bad904195112210302'
-            HttpResponse<Map> rsp = getTemperatureFromRemoteApi(cityAndState, weatherApiKey)
-            Map responseMap = getMapFromHttpResponseWhileHandleNullResponseUseCase(cityAndState, rsp)
-            Temperature tempDomain = getTemperatureDomainFromHttpResponse(responseMap)
-            tempDomain
-        }
-
-        private Map getMapFromHttpResponseWhileHandleNullResponseUseCase(Map cityAndState, HttpResponse<Map> rsp) {
-            Map map = [:]
-            if (Objects.isNull(rsp)) {
-                final Double ABSOLUTE_ZERO = -273.15d
-                final def errorUseCaseReferenceMap =
-                        [status: HttpStatus.I_AM_A_TEAPOT, current: [temp_c: ABSOLUTE_ZERO]]
-                map.location = [name: cityAndState.city, region: cityAndState.state]
-                map.putAll(errorUseCaseReferenceMap)
-            } else {
-                map.putAll(rsp.body())
-                map.status = rsp.status()
-            }
-            return map
-        }
-
-        private Temperature getTemperatureDomainFromHttpResponse(Map body) {
-            Map location = body.location as Map
-            Map current = body.current as Map
-            Map tempPayloadMap = [city: location.name, state: location.region, temp: current.temp_c]
-            Map tempConstructorMap = [description: "${tempPayloadMap.city}|${tempPayloadMap.state}", payload:
-                    tempPayloadMap]
-            Temperature tempDomain = new Temperature(tempConstructorMap)
-            tempDomain
-        }
-
-        private HttpResponse<Map> getTemperatureFromRemoteApi(Map cityAndState, weatherApiKey) {
-            def cityStateQueryParameter = "${cityAndState.city},${cityAndState.state}".replaceAll(/\s/, '%20')
-            log.info(cityStateQueryParameter)
-            HttpResponse<Map> rsp = null
-            try {
-                HttpRequest request = HttpRequest.GET("/current.json?key=${weatherApiKey}&q=${cityStateQueryParameter}")
-                rsp = getClient().toBlocking().exchange(request, Argument.of(HashMap.class as Class<Object>)) as HttpResponse<Map>
-            } catch (Exception ex) {
-                log.error("temperature lookup failure", ex)
-            }
-            rsp
         }
 
         private OrgTreeSupplier getOrgTreeSupplier() {
